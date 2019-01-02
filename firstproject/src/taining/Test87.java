bee44f18cd5e8f1bd3fe24ad5dde0d845fdb3870
Msasi2726 <46027729+Msasi2726@users.noreply.github.com>
2019-01-02 21:05:29 +0530

    two

--- firstproject/src/taining/Test87.java
+++ firstproject/src/taining/Test87.java
@@ -6,6 +6,7 @@ public class Test87
 {
 	public static void main(String[] args)
 	{
+		//use scanner
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a number");
 		 String x=sc.nextLine();
